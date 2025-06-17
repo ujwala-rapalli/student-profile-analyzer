d32087f414f0d64d4673f02377e32de4e89cdd7f
ujwala-rapalli <22211a6697@bvrit.ac.in>
2025-06-17 15:49:48 +0530

    Initial commit

--- app.py
+++ app.py
@@ -71,5 +71,8 @@ def predict():
         print(f"Error occurred: {str(e)}")
         return jsonify({'error': str(e)}), 400
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
